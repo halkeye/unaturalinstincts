7d9afccdd31483cb337c79a4711e5a7d6a347f33
halkeye <halkeye@9617410d-840b-0410-9f80-f73a7074d746>
2006-01-29 20:20:05 +0000

    working
    git-svn-id: http://svn.kodekoan.com/svn/unaturalinstincts@5 9617410d-840b-0410-9f80-f73a7074d746

--- trunk/src/Application.cpp
+++ trunk/src/Application.cpp
@@ -6,7 +6,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/* socket stuff */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+
 #include <set>
+
+#ifndef MAX
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#endif
 
 class Connection {
    public:
@@ -16,33 +26,62 @@ class Connection {
 class Player : public Connection {
    public:
       int fd;
+
+   public:
+      Player(int nfd) : fd(nfd) {}
+      void onRead() {
+         write(fd, "hi\n", 3);
+      }
 };
 
 class Application {
+   public:
+      Application(int port);
+
    private:
-      fd_set masterFds;
-      set<Player *> connections;
-      int maxFds;
+      int _control; /* Main server connection */
+      fd_set _masterFds;
+      std::set<Player *> players;
+      int _maxFds;
       bool shutdown;
+      int _port;
 
    public:
       void gameLoop();
       void onRead();
+      int makeSocket(int socktype);
+      bool setNonBlocking(int fd);
+
+      int port() { return _port; }
+      int port(int nport) { _port = nport; return _port; }
+
 };
 
 void Application::gameLoop() {
    fd_set readFds;
    int ret;
+   time_t tTime = 0;
    struct timeval tv;
    Player * player;
-   set<Player *>::iterator iter;
+   std::set<Player *>::iterator iter;
 
    shutdown = false;
+   FD_ZERO(&readFds);
+   FD_SET(_control,&_masterFds);
 
    while (!shutdown) {
+      memcpy(&readFds,&_masterFds,sizeof(readFds));
+
       tv.tv_sec = 1;
-      tv.tv_sec = 0;
-      ret = select(maxFds, &readFds, NULL, NULL, &tv);
+      tv.tv_usec = 0;
+        
+      /* Update pulse called every second */
+      if ( (time(0) - tTime) >= 1 /* second */ ) {
+         //UpdateHandler();
+         tTime = time(0);
+      }
+
+      ret = select(_maxFds+10, &readFds, NULL, NULL, &tv);
 
       if (ret == -1) {
          perror("gameLoop::select()");
@@ -54,7 +93,7 @@ void Application::gameLoop() {
          continue;
       }
 
-      if (FD_ISSET(control, &readFds)) {
+      if (FD_ISSET(_control, &readFds)) {
          this->onRead();
       }
 
@@ -65,9 +104,96 @@ void Application::gameLoop() {
          }
       }
    }
-   return 0;
+   return;
+}
+
+void Application::onRead() {
+   int new_fd;
+   struct sockaddr_in stSa;
+   unsigned int iSaSize=sizeof(stSa);
+
+   new_fd = accept(_control, (struct sockaddr*)&stSa,&iSaSize);
+   if (new_fd < 0) {
+      perror("accept()");
+      throw("accept");
+   }
+   setNonBlocking(new_fd);
+   FD_SET(new_fd, &_masterFds); 
+   _maxFds = MAX(_maxFds, new_fd+1);
+   Player * p = new Player(new_fd);
+	players.insert(p);
 }
 
 void Connection::close() {
    /* Remove me from Appliaction->fds */
 }
+
+int main(int argc, char ** argv) {
+   int port = 0;
+
+   if (argc > 1) {
+      port = atoi(argv[1]);
+   }
+      
+   if (port == 0) { 
+      port = 10000;
+   }
+   Application * app = new Application(port);
+   app->gameLoop();
+   return 0;
+}
+
+int Application::makeSocket(int socktype) {
+   int ret = socket(PF_INET, socktype, 0);
+   if (ret < 0) {
+      return -1;
+   }
+   return ret;
+}
+
+Application::Application(int port) {
+   struct sockaddr_in name;
+   int yes = 1;
+   _port = port;
+   _maxFds = 1;
+
+   _control = makeSocket(SOCK_STREAM);
+   if (!_control) {
+      perror("makeSocket");
+      throw("Unable to make a socket");
+   }
+   if (setsockopt(_control, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+   {
+      perror("setsockopt");
+      exit(1);
+   }
+
+
+   name.sin_family=AF_INET;
+   name.sin_port=htons(port);
+   name.sin_addr.s_addr=htonl(INADDR_ANY);
+
+   if (bind(_control, (sockaddr *)&name, sizeof(name))<0) {
+      perror("bind");
+      throw("bind");
+   }
+   setNonBlocking(_control);
+   if (listen(_control, SOMAXCONN) == -1)
+   {
+      perror("listen");
+      exit(1);
+   }
+
+   FD_ZERO(&_masterFds);
+   FD_SET(_control, &_masterFds);
+}
+
+bool Application::setNonBlocking(int fd)
+{
+	int oldflags=fcntl(fd, F_GETFL, 0);
+	if (oldflags == -1)
+		return true;
+   oldflags |= O_NONBLOCK;
+	return fcntl(fd, F_SETFL, oldflags);
+}
+
